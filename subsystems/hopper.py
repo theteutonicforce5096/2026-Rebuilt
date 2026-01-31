6662fe120a9d779d43230d1a438ea826833e679a
Riley Satterfield <lennoxlilac@gmail.com>
2026-01-31 12:31:11 -0600

    Still configuring stuff but I think I got it

---
+++ subsystems/hopper.py
@@ -0,0 +1,26 @@
+'''
+TODO: I am making the hopper and the intake two seperate things so it will make everything easier
+'''
+import wpilib
+import phoenix6
+import phoenix6.controls
+import phoenix6.hardware
+import phoenix6.configs
+import phoenix6.signals
+
+class Hopper:
+
+    def __init__(self):
+        # The device and canbus will have to be changed when I have more information
+        self.talon_motor = phoenix6.hardware.TalonFXS(device_id=0,canbus='rio')
+        self.talon_config = phoenix6.configs.talon_fxs_configs.TalonFXSConfiguration()
+        # I got this okay I think I am going to configure this maybe but alsg
+        self.talon_config.future_proof_configs = True
+        self.talon_config.motor_output = phoenix6.configs.config_groups.MotorOutputConfigs.
+        #TODO: I was in the middle of configuring this you gotta reference talon config each time.
+        
+    def hopper_on(self):
+        self.talon_motor.set(1)
+        self.talon_motor.
+
+
