ea80786df739d53d862129bbfa68fcfd4340d6f7
Riley Satterfield <lennoxlilac@gmail.com>
2026-03-02 18:27:25 -0600

    It works thanks jay I need indent rainbow

--- subsystems/intake.py
+++ subsystems/intake.py
@@ -16,7 +16,7 @@ from commands2.cmd import print_
 
 class Intake(Subsystem): # <-- Telling subsystem that its part of it too
 
-    def __init__(self, CAN_ID):
+    def __init__(self):
         # Note to Riley:
         # The reason we need Subsystem.__init__ is because 
         # it needs to know the parent class's setup code and 
@@ -29,15 +29,15 @@ class Intake(Subsystem): # <-- Telling subsystem that its part of it too
         # self.Neo550_1(self.Neo_configs)
 
         # The device and canbus will have to be changed when I have more information
-        self.talon_motor2 = TalonFXS(CAN_ID)
+        self.talon_motor2 = TalonFXS(80)
         self.talon_config2 = phoenix6.configs.talon_fxs_configs.TalonFXSConfiguration()
         
 
-        def intake_running(self):
+    def intake_running(self):
             self.talon_motor2.set(0.5) # Set to 50% power, can be changed later
             # print("Intake is running!!!")
 
-        def intake_stopped(self):
+    def intake_stopped(self):
             self.talon_motor2.set(0) # Stop the motor
             
    
