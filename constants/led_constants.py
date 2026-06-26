b4f31cb13668ba02f1a8bcee4ec8888efb5ca4d6
Jay Ticku <jay.ticku@gmail.com>
2026-06-25 21:57:24 -0500

    Oops I forgot to add these

---
+++ constants/led_constants.py
@@ -0,0 +1,43 @@
+from subsystems import led
+from subsystems.led import LED
+from phoenix6 import hardware
+from phoenix6 import CANBus
+from phoenix6.configs import TalonFXSConfiguration
+from phoenix6.configs import CANdleConfiguration
+from phoenix6.configs.candle_configs import LEDConfigs
+from commands2 import Subsystem
+import phoenix6
+from phoenix6.signals import StripTypeValue
+
+
+class LEDConstants(Subsystem):
+    """
+    Constants for LED Subsystem
+    """
+
+    # CANBus instance
+    _canbus = CANBus("Drivetrain")
+
+    # CAN IDs
+    _led_id = 3
+
+    # Number of times to attempt to configure each device
+    _num_config_attempts = 3
+
+    # LED Configs (CANdle - TalonFXS)
+    _led_configs = CANdleConfiguration()
+    _led_configs.led.with_strip_type(phoenix6.signals.StripTypeValue.RGB)
+
+
+    @classmethod
+    def create_led(cls) -> LED:
+        """
+        Creates an LED subsystem instance.
+        """
+
+        return LED(
+            cls._canbus,
+            cls._led_id,
+            cls._led_configs,
+            cls._num_config_attempts
+        )
